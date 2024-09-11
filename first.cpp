50b0b429c0c1542111ae1d8dd91efda026c6776b
Bharath Kumar J N <bharathdhanavan2612@gmail.com>
2024-09-11 21:27:06 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+  int a = 20;
+  float f = 3.14;
+
+  string s = "Hello World!";
+  cout << a << endl;
+  cout << f << endl;
+  cout << s << endl;
+}
